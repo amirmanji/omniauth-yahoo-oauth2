3ff0adc7c2318e72d40812e97f839e5b122dc8a6
nicholas a. evans <nick@ekenosen.net>
2015-06-29 16:15:56 -0400

    Add first_name, last_name, email (standard omniauth schema)
    https://github.com/intridea/omniauth/wiki/Auth-Hash-Schema

--- lib/omniauth/strategies/yahoo_oauth2.rb
+++ lib/omniauth/strategies/yahoo_oauth2.rb
@@ -16,8 +16,10 @@ module OmniAuth
 
       info do
         {
-          name: raw_info['profile']['nickname'],
           nickname: raw_info['profile']['nickname'],
+          first_name: raw_info["givenName"],
+          last_name:  raw_info["familyName"],
+          email: preferred_email,
           gender: raw_info['profile']['gender'],
           language: raw_info['profile']['lang'],
           location: raw_info['profile']['location'],
@@ -44,6 +46,13 @@ module OmniAuth
         raw_info_url = "https://social.yahooapis.com/v1/user/#{uid}/profile?format=json"
         @raw_info ||= access_token.get(raw_info_url).parsed
       end
+
+      def preferred_email
+        emails = raw_info["profile"].fetch("emails",[])
+        email  = emails.find {|e| e["primary"] } || emails.first
+        email && email["handle"]
+      end
+
     end
   end
 end
