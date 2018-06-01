202d38f7d88474935c2f316e3f357f7230f9ce4d
Fabian Jäger <fabian@mailbutler.io>
2018-06-01 15:26:39 +0200

    style improvements

--- lib/omniauth/strategies/yahoo_oauth2.rb
+++ lib/omniauth/strategies/yahoo_oauth2.rb
@@ -5,12 +5,11 @@ module OmniAuth
   module Strategies
     class YahooOauth2 < OmniAuth::Strategies::OAuth2
       option :name, 'yahoo_oauth2'
-      
-      option :client_options, {
-        site: 'https://api.login.yahoo.com',
-        authorize_url: '/oauth2/request_auth',
-        token_url: '/oauth2/get_token',
-      }
+
+      option :client_options,
+             site: 'https://api.login.yahoo.com',
+             authorize_url: '/oauth2/request_auth',
+             token_url: '/oauth2/get_token'
 
       uid { access_token.params['xoauth_yahoo_guid'] }
 
@@ -27,23 +26,24 @@ module OmniAuth
           }
         }
       end
-      
+
       def callback_url
         options[:redirect_uri] || (full_host + script_name + callback_path)
       end
 
       def build_access_token
         verifier = request.params['code']
-        
+
         auth = "Basic #{Base64.strict_encode64("#{options.client_id}:#{options.client_secret}")}"
-        
+
         token = client.get_token(
-          { redirect_uri: callback_url, code: verifier, grant_type: 'authorization_code', headers: { 'Authorization' => auth } }.
-          merge(token_params.to_hash(symbolize_keys: true)), deep_symbolize(options.auth_token_params))
-        
+          { redirect_uri: callback_url, code: verifier, grant_type: 'authorization_code', headers: { 'Authorization' => auth } }
+          .merge(token_params.to_hash(symbolize_keys: true)), deep_symbolize(options.auth_token_params)
+        )
+
         token
       end
-      
+
       def raw_info
         raw_info_url = "https://social.yahooapis.com/v1/user/#{uid}/profile?format=json"
         @raw_info ||= access_token.get(raw_info_url).parsed
